ef03458c065d1a6df394aab529daa0b8b20ebecf
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-09-29 17:23:37 -0600

    finally have pages rendering corectly and connected to the router

--- src/components/ResponsiveAppBar.js
+++ src/components/ResponsiveAppBar.js
@@ -1,12 +1,11 @@
 import * as React from 'react';
-import { Link } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+// import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+// import IconButton from '@mui/material/IconButton';
+// import MenuIcon from '@mui/icons-material/Menu';
 
 
 export default function ButtonAppBar() {
@@ -14,21 +13,9 @@ export default function ButtonAppBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button href='/'  to="/" color="inherit">Home</Button>
-          <Button href='/login'  to="/login" color="inherit">Login</Button>
-          <Button href='/signup'  to="/signup" color="inherit">Signup</Button>
+          <Button to="/" color="inherit">Home</Button>
+          <Button   to="/login" color="inherit">Login</Button>
+          <Button   to="/signup" color="inherit">Signup</Button>
         </Toolbar>
       </AppBar>
     </Box>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Home from './pages/Home';
+import * as React from "react";
+import ReactDOM from "react-dom/client";
+import {Link} from "react-router-dom";
+// import Home from './pages/Home';
 import Login from "./pages/Login";
 import SignUp from './pages/SignUp';
 import "./App.css";
@@ -9,28 +10,69 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate
+  
 } from "react-router-dom";
-import ButtonAppBar from './components/ResponsiveAppBar';
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
-  <ButtonAppBar />
-  <Router>
-    <Routes>
+
+    // <nav className="nav-container">
+    //   <ul>
+    //     <li>
+    //       <Link to="/">Home</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/login">Login</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/sigup">Signup</Link>
+    //     </li>
+    //   </ul>
+    // </nav>
+    <Router>
+      <NavList />
+      <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
-        <Route path="/user" element={<Navigate to="/login" />}></Route>
+
         <Route path="/signup" element={<SignUp />}></Route>
       </Routes>
-  </Router>
-  </>
+    </Router>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
+function Home() {
+  return (
+    <div>
+      <h1>Home Route</h1>
+    </div>
+  );
+}
+// function SignUp() {
+//   return (
+//     <div>
+//       <h1>Signup Route</h1>
+//     </div>
+//   );
+// }
+// function Login() {
+//   return (
+//     <div>
+//       <h1>Login Route</h1>
+//     </div>
+//   );
+// }
+function NavList() {
+  return(
+    <div className="nav-container">
+      <Link to="/" style={{padding: 25, textDecoration: 'none', color:"white", fontWeight:"bold", fontSize:"large"}}>Home</Link>
+      <Link to="/login" style={{padding: 25, textDecoration: 'none', color:"white", fontWeight:"bold", fontSize:"large"}}>SignIn</Link>
+      <Link to="/signup" style={{padding: 25, textDecoration: 'none', color:"white", fontWeight:"bold", fontSize:"large"}}>SignUp</Link>
+    </div>
+  )
+
+}
