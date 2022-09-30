449965b35898b1c789b0d7b84bd3aca2ef19528a
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-09-30 11:26:22 -0600

    data workable and dynamic rendering to home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,50 @@
-import React from "react";
-import Button from '@mui/material/Button';
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
 
-
-const API_URL = "https://api.openbrewerydb.org/breweries?by_state=colorado&per_page=20";
+const API_URL =
+  "https://api.openbrewerydb.org/breweries?by_state=colorado&per_page=20";
 
 export default function Home() {
+  const [pubData, setPubData] = useState([]);
 
-  const searchBreweries = async (title) => {
+  // useEffect(() => {
+  //   const fetchData = async (by_state) => {
+  //     const response = await fetch(`${API_URL}`);
+  //     const data = await response.json();
+  //     setPubData(data.slice(0, 20));
+  //   };
+  //   fetchData();
+  // }, []);
+  const searchBreweries = async (by_state) => {
     const response = await fetch(`${API_URL}`);
     const data = await response.json();
-    console.log(data);
+    setPubData(data);
+    // console.log(data);
+    // console.log(data[0].id);
+
+    pubData.forEach((data) => {
+      console.log(data);
+    })
+
   }
   return (
     <div>
-      <h1>Home page</h1>
-      <Button onClick={() => {
-        searchBreweries();
-      }}>Search</Button>
+      {pubData.map((pubData, id) => (
+        <div key={id}>
+          <p>{pubData.name}</p>
+          <p>{pubData.city}</p>
+          <p>{pubData.street}</p>
+          <p>{pubData.brewery_type}</p>
+        </div>
+      ))}
+      <h1>{pubData.name}</h1>
+      <Button
+        onClick={() => {
+          searchBreweries();
+        }}
+      >
+        Search
+      </Button>
     </div>
   );
 }
