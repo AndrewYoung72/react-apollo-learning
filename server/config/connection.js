5940f4a1d69e888ce39d34cedd3f55e9e4a91ec4
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-09-30 16:35:30 -0600

    express server connected

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,8 @@ import pkg from 'mongoose';
 const { connect, connection } = pkg;
 
 connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/react-apollo-learning",
-  // process.env.MONGODB_URI || "mongodb+srv://AndrewYoung72:andy12345y@cluster0.vcwmv.mongodb.net/bikesDB?retryWrites=true&w=majority",
+  // process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/react-apollo-learning",
+  process.env.MONGODB_URI || "mongodb+srv://AndrewYoung72:andy12345y@cluster0.vcwmv.mongodb.net/beerDB?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
