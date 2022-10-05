aa79b420d43e5d0e59e9b0f8fc25b883e1627ad0
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-10-05 15:53:48 -0600

    typedefs set

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,35 @@
+import { gql } from "apollo-server-express";
+
+const typeDefs = gql`
+  type Favorite {
+    _id: ID
+    name: String
+    brewery_type: String
+    city: String
+    street: String
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    favorites: [Favorite]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    user: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addFavorite( name: String!, brewery_type: String!, city: String!, street: String!): Favorite
+    
+  }
+`;
+
+export default typeDefs;
